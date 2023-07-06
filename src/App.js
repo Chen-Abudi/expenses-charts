cd0968440f2ccc526d63c5e9e44072c94b0f2d96
Grace Chen Abudi <cheezumcali23@gmail.com>
2023-07-06 18:44:38 +0300

    dev: basic initial component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>Aloha</p>
+      <ExpenseItem />
     </div>
   );
 }
