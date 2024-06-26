632b59350c007eca2c37661f504a5b34f6c5c7dd
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-26 18:58:58 +0300

    refactor: Output expenses code into separate file

---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses({ items }) {
+  return (
+    <div className="expenses">
+      {items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
---
+++ src/data/expenses.js
@@ -0,0 +1,26 @@
+export const expenses = [
+  {
+    id: 1,
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 2,
+    title: "Groceries",
+    amount: 37.5,
+    date: new Date(2021, 5, 14),
+  },
+  {
+    id: 3,
+    title: "Book",
+    amount: 12.99,
+    date: new Date(2021, 8, 10),
+  },
+  {
+    id: 4,
+    title: "Dining Table",
+    amount: 59.99,
+    date: new Date(2020, 3, 5),
+  },
+];
