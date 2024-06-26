c90c376d5d0fd9144a8697513dba91cea7f08980
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-26 19:24:02 +0300

    chore: Clear comments

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -13,7 +13,6 @@ function ExpenseItem(props) {
         <div>{year}</div>
         <div>{day}</div>
       </div>
-      {/* {props.date.toISOString()} */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
